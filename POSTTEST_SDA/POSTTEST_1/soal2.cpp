0961adfa1925634fa442cce6067176232cfb6df5
MTHermawan <trigusnihermawan@gmail.com>
2025-09-12 16:08:32 +0800

    Menampilkan matriks di soal 2

--- POSTTEST_SDA/POSTTEST_1/soal2.cpp
+++ POSTTEST_SDA/POSTTEST_1/soal2.cpp
@@ -8,14 +8,24 @@ int main()
                           {7, 8, 9} };
 
     int jumlahBarisKedua = 0;
+    int panjangBaris = sizeof(matriks) / sizeof(matriks[0]);
     int panjangKolom = sizeof(matriks[0]) / sizeof(matriks[0][0]);
 
     for (int i = 0; i < panjangKolom; i++)
-    {
         jumlahBarisKedua += matriks[1][i];
+    
+    for (int i = 0; i < panjangBaris; i++)
+    {
+        for (int j = 0; j < panjangKolom; j++)
+        {
+            cout << matriks[i][j];
+            if (j < panjangKolom - 1)
+                cout << " ";
+        }
+        cout << endl;
     }
 
-    cout << "Jumlah baris kedua matriks adalah " << jumlahBarisKedua;
+    cout << "Hasil penjumlahan baris kedua matriks di atas adalah " << jumlahBarisKedua;
 
     return 0;
 }
